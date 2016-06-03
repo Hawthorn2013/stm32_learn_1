a03f1338dd5379052c8b4253e11a6461dee1164d
Hawthorn2013 <mynetuser@outlook.com>
2016-06-03 23:24:46 +0800

    闪烁LED弄好了
    * 延时函数用的循环，太粗糙。

---
+++ source/main.c
@@ -0,0 +1,50 @@
+#include <stm32f10x.h>
+#include <GPIO_STM32F10x.h>
+#include <stm32f10x_rcc.h>
+#include <stm32f10x_conf.h>
+#include <stm32f10x_gpio.h>
+#include <stm32f10x_rtc.h>
+
+void Delay_us(uint32_t us)
+{
+	volatile uint16_t us_i = us;
+	while(us_i--)
+		;
+}
+
+int main(void)
+{
+	volatile int i = 0;
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  GPIO_InitStructure.GPIO_Pin = 0XFFFF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+	while(1)
+  {
+		GPIO_Write(GPIOA, 0xFFFF);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		GPIO_Write(GPIOA, 0);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		Delay_us(0xffffffff);
+		i++;
+	}
+}
