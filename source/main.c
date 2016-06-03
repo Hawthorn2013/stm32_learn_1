e0c1115e78f0261bb821cab1b3ac4334b33c8674
Hawthorn2013 <mynetuser@outlook.com>
2016-06-04 02:16:35 +0800

    SPI发送信号正常
    - 时钟空闲为低
    * 采样沿未测
    * MSB或LSB未测

--- source/main.c
+++ source/main.c
@@ -19,11 +19,20 @@ void Send_To_Console(const char *data, uint32_t len)
     }
 }
 
+void OLED_SendData(uint8_t data)
+{
+    while((SPI2->SR & SPI_I2S_FLAG_TXE) == RESET);  //等待发送区空  
+    SPI2->DR = data;   //发送一个byte  
+    while((SPI2->SR & SPI_I2S_FLAG_RXNE) == RESET);//等待接收完一个byte 
+    (void)(SPI2->DR);                //返回收到的数据   
+}
+
 int main(void)
 {
     volatile int i = 0;
     GPIO_InitTypeDef GPIO_InitStructure;
     USART_InitTypeDef USART_InitStructure;
+    SPI_InitTypeDef  SPI_InitStructure;
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
@@ -42,11 +51,31 @@ int main(void)
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
     USART_Init(USART1, &USART_InitStructure);
     USART_Cmd(USART1, ENABLE);                    //使能串口
+    //SPI配置
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO|RCC_APB2Periph_GPIOB, ENABLE);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 |GPIO_Pin_14| GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;  //复用推挽输出
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2,ENABLE);
+    SPI_Cmd(SPI2, DISABLE);            //必须先禁能,才能改变MODE
+    SPI_InitStructure.SPI_Direction =SPI_Direction_2Lines_FullDuplex;  //两线全双工
+    SPI_InitStructure.SPI_Mode =SPI_Mode_Master;       //主
+    SPI_InitStructure.SPI_DataSize =SPI_DataSize_8b;      //8位
+    SPI_InitStructure.SPI_CPOL =SPI_CPOL_Low;        //CPOL=1时钟悬空低
+    SPI_InitStructure.SPI_CPHA =SPI_CPHA_1Edge;       //CPHA=1 数据捕获第1个
+    SPI_InitStructure.SPI_NSS =SPI_NSS_Soft;        //软件NSS
+    SPI_InitStructure.SPI_BaudRatePrescaler =SPI_BaudRatePrescaler_256;  //2分频
+    SPI_InitStructure.SPI_FirstBit =SPI_FirstBit_MSB;      //高位在前
+    SPI_InitStructure.SPI_CRCPolynomial =7;        //CRC7
+    SPI_Init(SPI2,&SPI_InitStructure);
+    SPI_Cmd(SPI2, ENABLE);
     while(1)
     {
         char dst[50];
         sprintf(dst, "%d + %f = %f\r\n", i, 7.4, i + 7.4);
         Send_To_Console(dst, strlen(dst));
         i++;
+        OLED_SendData(0x0a);
     }
 }
