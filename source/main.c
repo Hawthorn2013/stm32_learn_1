358157abd2df700d5eb59b4dfaf20925087ee774
Hawthorn2013 <mynetuser@outlook.com>
2016-06-04 01:08:20 +0800

    串口改用USART1
    - PA9-TX, PA10-RX.
    - 为了方便使用蓝牙。

--- source/main.c
+++ source/main.c
@@ -12,32 +12,28 @@ int main(void)
     volatile int i = 0;
     GPIO_InitTypeDef GPIO_InitStructure;
     USART_InitTypeDef USART_InitStructure;
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
-    USART_DeInit(USART2);
-    //重映射端口
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_AFIO, ENABLE);
-    GPIO_PinRemapConfig(GPIO_Remap_USART2, ENABLE);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
     //USART 初始化设置
+    USART_DeInit(USART1);
     USART_InitStructure.USART_BaudRate = 38400;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
     USART_InitStructure.USART_StopBits = USART_StopBits_1;
     USART_InitStructure.USART_Parity = USART_Parity_No;
     USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-    USART_Init(USART2, &USART_InitStructure);
-    USART_Cmd(USART2, ENABLE);                    //使能串口
+    USART_Init(USART1, &USART_InitStructure);
+    USART_Cmd(USART1, ENABLE);                    //使能串口
     while(1)
     {
         i++;
-        USART_SendData(USART2, 0xAA);
-        while(USART_GetFlagStatus(USART2,USART_FLAG_TC) != SET);//等待发送结束
+        USART_SendData(USART1, 0xAA);
+        while(USART_GetFlagStatus(USART1,USART_FLAG_TC) != SET);//等待发送结束
     }
 }
